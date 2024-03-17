5e0e0a9f2bdb55e0da5b5f6d9648108027c49b27
ImArbeen <137374826+ImArbeen@users.noreply.github.com>
2024-03-17 14:58:05 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// Making Calculator Practice 
+
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
